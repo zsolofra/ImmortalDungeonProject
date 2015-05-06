1d431f3cb8678fb59f47e44b6b959e45844f387a
Zach Solofra <zsolofra@gmail.com>
2015-05-06 12:13:50 -0500

    first commit

---
+++ Assets/Custom Assets/Scripts/menuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public Canvas ExitMenu;
+	public Button startText;
+	public Button exitText;
+
+
+	// Use this for initialization
+	void Start () {
+
+		ExitMenu = ExitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		ExitMenu.enabled = false;
+
+	
+	}
+	
+
+	public void ExitPress()
+	{
+		ExitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress()
+	{
+		ExitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartLevel()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit ();
+	}
+
+}
---
+++ Assets/CustomScripts/PlayerCollisions.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerCollisions : MonoBehaviour 
+{
+
+	bool triggered = false;
+	int  numLit = 0;
+	public GameObject textGameObject;
+	public GameObject obstructionGameObject;
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Fire") {
+
+
+			if (triggered == false) {
+				Debug.Log("triggered");
+				triggered = true;
+				numLit ++;
+				Debug.Log(numLit);
+				col.GetComponent<ParticleSystem>().enableEmission = true;
+			}
+
+		} //end of if its a fire
+
+		if (col.gameObject.tag == "Obstruction") {
+
+			if (numLit >= 4) {
+			col.GetComponent<ParticleSystem>().enableEmission = true;
+				StartCoroutine(Wait(5.0f));
+
+			}else {
+			Text text = textGameObject.GetComponent<Text>();
+			text.text = "Light the Fires to Pass";
+			}
+
+
+
+		} //end of if its a obstruction
+
+	}
+
+
+	void OnTriggerExit (Collider col) {
+		if (col.gameObject.tag == "Fire") {
+			if (triggered == true) {
+				Debug.Log ("untriggered");
+				triggered = false;
+			}
+		}
+
+		if (col.gameObject.tag == "Obstruction") {
+			
+			Text text = textGameObject.GetComponent<Text>();
+			text.text = "";
+
+
+		}
+	}
+
+	private IEnumerator Wait(float seconds)
+	{
+		Debug.Log("waiting");
+		yield return new WaitForSeconds(seconds);
+
+		Debug.Log("Booom");
+		Destroy(obstructionGameObject);
+	}
+}
